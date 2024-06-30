06b883dee06772f5c10f388c8133b0b70c59c285
Rophah <aderafat@gmail.com>
2024-06-30 15:14:59 +0100

    correction

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,19 +3,21 @@ import styles from "./home.module.css"
 
 const Home = () => {
   return (
-  <div className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Creative Thoughts Agency</h1>
-        <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi cupiditate eum assumenda. Aliquam, nulla. Velit, voluptatum dolor nesciunt possimus tenetur, iusto neque ullam sequi molestiae, animi fuga quis exercitationem! Quisquam?</p>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
         <div className={styles.buttons}>
           <button className={styles.button}>Learn More</button>
           <button className={styles.button}>Contact</button>
         </div>
         <div className={styles.brands}>
-          <Image src="/brands.png" alt="" fill className="styles.brand"/>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
         </div>
       </div>
-      
       <div className={styles.imgContainer}>
         <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
       </div>
